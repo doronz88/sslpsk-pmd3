d221c479c6826e92db2c476983736b77869ce4d4
user <user@users-MacBook-Pro.local>
2024-02-09 17:25:45 +0200

    sort imports

--- sslpsk_pmd3/__init__.py
+++ sslpsk_pmd3/__init__.py
@@ -12,4 +12,4 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License
 
-from sslpsk_pmd3.sslpsk import wrap_socket, SSLPSKContext
+from sslpsk_pmd3.sslpsk import SSLPSKContext, wrap_socket
